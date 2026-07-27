3c84046282b84d5cc14a0f39b5c7cfad76ec8eaa
meshach5667 <meshachzakumi@gmail.com>
2026-07-27 21:00:05 +0100

    apk

--- aipal/apps/mobile/android/app/build.gradle.kts
+++ aipal/apps/mobile/android/app/build.gradle.kts
@@ -7,7 +7,7 @@ plugins {
 android {
     namespace = "io.aipal.mvp"
     compileSdk = flutter.compileSdkVersion
-    ndkVersion = flutter.ndkVersion
+    ndkVersion = "27.0.12077973"
 
     compileOptions {
         isCoreLibraryDesugaringEnabled = true
--- aipal/apps/mobile/android/build.gradle.kts
+++ aipal/apps/mobile/android/build.gradle.kts
@@ -14,6 +14,14 @@ rootProject.layout.buildDirectory.value(newBuildDir)
 subprojects {
     val newSubprojectBuildDir: Directory = newBuildDir.dir(project.name)
     project.layout.buildDirectory.value(newSubprojectBuildDir)
+    afterEvaluate {
+        if (plugins.hasPlugin("com.android.application") || plugins.hasPlugin("com.android.library")) {
+            val androidExtension = extensions.findByName("android")
+            if (androidExtension is com.android.build.gradle.BaseExtension) {
+                androidExtension.ndkVersion = "27.0.12077973"
+            }
+        }
+    }
 }
 tasks.register<Delete>("clean") {
     delete(rootProject.layout.buildDirectory)
